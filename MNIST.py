4900e18719ff8243a25a1d9db82ea36e261b6b7a
sciNtech <sntpresident@gmail.com>
2018-07-06 23:08:40 +0800

    Updated MNIST TfCV

--- MNIST.py
+++ MNIST.py
@@ -1,9 +1,11 @@
 import numpy as np
 from keras.datasets import mnist
+from sklearn.metrics import accuracy_score
 from keras.utils import to_categorical
+from scipy import stats
 
-class MNIST():
-    def load(self, threeDim=False):
+class PMNIST():
+    def load_pmnist(self, threeDim=False):
         (xtrain, ytrain), (xtest, ytest) = mnist.load_data()
         
         if not threeDim:
@@ -30,7 +32,7 @@ class MNIST():
         y.append(ytest)
         
         X = np.concatenate((xtrain, xtest)).reshape([len(xtrain)+len(xtest), 784])
-        y = np.concatenate((ytrain, ytest))
+        y = np.concatenate((ytrain, ytest)).reshape([len(xtrain)+len(xtest), 10])
         
         complete_trial_X = np.array_split(X, folds) # Complete trial consisting of 5 folds on the dataset
         complete_trial_y = np.array_split(y, folds) # Complete trial consisting of 5 folds on the dataset
@@ -45,7 +47,51 @@ class MNIST():
         trials_X = np.array(trials_X) # Complete set of 5 trials per model for all 20 kNN models
         trials_y = np.array(trials_y) # Complete set of 5 trials per model for all 20 kNN models
         
-        print ('{} and {}'.format(trials_X.shape, trials_y.shape)) # All 20 * 5 trials comprising of 5 folds per trial
+        return trials_X, trials_y
         
-        return trials_X, trials_y        
+    def get_chart_for_model(self, kNN, trialsX, trialsY):
+        accuracy_chart = []
+        test_index_X = 0
+        test_index_y = 0
+        for trial in range(len(trialsX)):
+            print ('Trial {}'.format(trial+1))
+            
+            current_trial_batch_X = trialsX[trial] # Each of the 5 trials per model with 5 folds each
+            current_trial_batch_y = trialsY[trial] # Each of the 5 trials per model with 5 folds each
+            print ('Total Trial batch: {} | {}'.format(current_trial_batch_X.shape, current_trial_batch_y.shape))
+            
+            # Per trial analysis
+            x_test_fold = current_trial_batch_X[test_index_X] # Test set X for each trial
+            y_test_fold = current_trial_batch_y[test_index_y] # Test set y for each trial
+            print ('Testing batch: {} | {}'.format(x_test_fold.shape, y_test_fold.shape))
+            
+            x_train_folds = np.delete(current_trial_batch_X, test_index_X, 0) # The current trial batch without the test fold
+            y_train_folds = np.delete(current_trial_batch_y, test_index_y, 0) # The current trial batch without the test fold
+            
+            # Merge training folds to unified array
+            x_train_fold = np.concatenate((x_train_folds))
+            y_train_fold = np.concatenate((y_train_folds))
+            print ('Training batch: {} | {}'.format(x_train_fold.shape, y_train_fold.shape))
+            
+            # Fitting current kNN Model with unified training set and obtaining accuracy by comparing predictions with ground truth
+            kNN.fit(x_train_fold, y_train_fold)
+            y_hat_fold = kNN.predict(x_test_fold)
+            accuracy = accuracy_score(y_hat_fold, y_test_fold)
+            print ('Accuracy for Trial {}: {}'.format(test_index_X+1, accuracy))
+            accuracy_chart.append(accuracy)
+            
+            test_index_X += 1 # Increment index by 1 to change test set X for next trial
+            test_index_y += 1 # Increment index by 1 to change test set y for next trial
+            print ()
+            
+        accuracy_chart = np.array(accuracy_chart)
+        
+        return accuracy_chart
+        
+    def get_pval_for_charts(self, Ac1, Ac2):
+        np.random.seed(0)
+        pval = stats.ttest_rel(Ac1, Ac2)[1]
+        
+        return pval
+        
     
--- main.py
+++ main.py
@@ -1,80 +1,53 @@
 import numpy as np
 from scipy import stats
 from pprint import pprint
-from MNIST import MNIST
+from MNIST import PMNIST
 
-from keras.datasets import mnist
-from keras.utils import to_categorical
 from keras.models import Sequential, model_from_json
-from keras.layers import Dense
+from keras.layers import Dense, Flatten, Conv2D, MaxPool2D
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 
-mnist = MNIST()
-xtrain, ytrain, xtest, ytest = mnist.load(threeDim=False)
+from benchmark import load_mnist, load_model_CNN, train_model
 
-# model = Sequential()
-# model.add(Dense(500, input_shape=[784,], activation='relu'))
-# model.add(Dense(250, activation='relu'))
-# model.add(Dense(125, activation='relu'))
-# model.add(Dense(10, activation='softmax'))
-# model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-# model.summary()
+# CNN
+xtrain, ytrain, xtest, ytest = load_mnist()
+model = load_model_CNN()
+trained_model, model_dict = train_model(model, xtrain, ytrain, xtest, ytest)
+print (model_dict['acc'])
 
-# model.fit(xtrain, ytrain, epochs=10, batch_size=64) # Untrained model
+y_hat_train = trained_model.predict(xtrain)
+y_hat_test = trained_model.predict(xtest)
 
-# model.load_weights("model.h5") # Loading pre-trained model
-# stats = model.evaluate(xtest, ytest, batch_size=64)
-# y_hat = model.predict(xtest)
-# accuracy = stats[1] * 100
-#
-# print (accuracy)
-
-def get_chart_for_model(kNN, trialsX, trialsY):
-    accuracy_chart = []
-    test_index_X = 0
-    test_index_y = 0
-    
-    for i in range(len(trialsX)):
-        print ('Trial {}'.format(i+1))
-        
-        current_trial_batch_X = trialsX[i] # Each of the 5 trials per model with 5 folds each
-        current_trial_batch_y = trialsY[i] # Each of the 5 trials per model with 5 folds each
-        print ('Total batch: {} | {}'.format(current_trial_batch_X.shape, current_trial_batch_y.shape))
-        
-        # Per trial analysis
-        x_test_fold = current_trial_batch_X[test_index_X] # Test set X for each trial
-        y_test_fold = current_trial_batch_y[test_index_y] # Test set y for each trial
-        print ('Testing batch: {} | {}'.format(x_test_fold.shape, y_test_fold.shape))
-        
-        x_train_folds = np.delete(current_trial_batch_X, test_index_X, 0) # The current trial batch without the test fold
-        y_train_folds = np.delete(current_trial_batch_y, test_index_y, 0) # The current trial batch without the test fold
-        
-        # Merge training folds to unified array
-        x_train_fold = np.concatenate((x_train_folds))
-        y_train_fold = np.concatenate((y_train_folds))
-        print ('Training batch: {} | {}'.format(x_train_fold.shape, y_train_fold.shape))
-        
-        # Fitting kNN Model with unified training set and obtaining accuracy by comparing predictions with ground truth labels
-        kNN.fit(x_train_fold, y_train_fold)
-        y_hat_fold = kNN.predict(x_test_fold)
-        accuracy = accuracy_score(y_hat_fold, y_test_fold)
-        print ('Accuracy for Trial {}: {}'.format(test_index_X+1, accuracy))
-        accuracy_chart.append(accuracy)
-        
-        test_index_X += 1 # Increment index by 1 to change test set X for next trial
-        test_index_y += 1 # Increment index by 1 to change test set y for next trial
-        print ()
-        
-    accuracy_chart = np.array(accuracy_chart)
-    
-    return accuracy_chart
+# k-NN
+accuracy_charts = []
+pmnist = PMNIST()
+xtrain, ytrain, xtest, ytest = pmnist.load_pmnist(threeDim=False)
 
 for kval in range(1, 21):
-    kNN = KNeighborsClassifier(n_neighbors=kval)
+    kNN = KNeighborsClassifier(n_neighbors=kval) # Generate kNN model with different k value
+    
     print ('For kNN {}'.format(kval))
-    trials_X, trials_y = mnist.serve_trials(xtrain, ytrain, xtest, ytest, 5)
-    accuracy_chart_for_model = get_chart_for_model(kNN, trials_X, trials_y) # Get accuracy chart of 5 accuracies per trial
+    trials_X, trials_y = pmnist.serve_trials(xtrain, np.round(y_hat_train), xtest, np.round(y_hat_test), 5)
+    accuracy_chart_for_model = pmnist.get_chart_for_model(kNN, trials_X, trials_y) # Get accuracy chart of 5 accuracies per trial
+    accuracy_charts.append(accuracy_chart_for_model)
+    
     print ('Accuracy chart for kNN {}: {}'.format(kval, accuracy_chart_for_model))
     print ('_________________________________________________________')
+    
+accuracy_charts = np.array(accuracy_charts)
+Pvals = []
+    
+for i in range(len(accuracy_charts)):
+    base_chart = accuracy_charts[i]
+    pvals_for_base_chart = []
+    for j in range(len(accuracy_charts)):
+        ref_chart = accuracy_charts[i][j]
+        pval_with_ref_chart = pmnist.get_pval_for_charts(base_chart, ref_chart) # Obtaining p value using paired t-test between base chart and reference chart
+        pvals_for_base_chart.append(pval_with_ref_chart)
+    Pvals.append(pvals_for_base_chart)
+    
+for i in range(len(Pvals)):
+    print (Pvals[i])
+        
